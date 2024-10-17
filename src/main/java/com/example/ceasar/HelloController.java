a90c57eb580455204d616d4d6c98e199c09ebfc6
EJ1DZE <trubarov.andrey@mail.ru>
2024-10-18 01:17:42 +0300

    Second

--- src/main/java/com/example/ceasar/HelloController.java
+++ src/main/java/com/example/ceasar/HelloController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ComboBox;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -136,37 +137,6 @@ public class HelloController {
             System.out.println("Введите нормальный путь");
         }
     }
-    public static boolean isValidText(String text) {
-        try {
-            URL url = new URL("https://api.languagetool.org/v2/check");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setDoOutput(true);
-
-            // Формируем данные запроса
-            String data = "text=" + text + "&language=en-US";  // Используйте "ru" для русского языка
-
-            try (OutputStream os = connection.getOutputStream()) {
-                os.write(data.getBytes());
-                os.flush();
-            }
-
-            // Читаем ответ
-            Scanner scanner = new Scanner(connection.getInputStream());
-            StringBuilder response = new StringBuilder();
-            while (scanner.hasNext()) {
-                response.append(scanner.nextLine());
-            }
-            scanner.close();
-
-            // Проверяем, есть ли ошибки
-            return !response.toString().contains("\"matches\":[]");  // Если ошибок нет, массив "matches" будет пуст
-        } catch (Exception e) {
-            System.out.println("Ошибка при обращении к API: " + e.getMessage());
-        }
-        return false;
-    }
 
     public void DecryptionButtonClick(ActionEvent actionEvent) {
         String vvod = inputForEncryption.getText().toUpperCase();
@@ -179,5 +149,20 @@ public class HelloController {
             WorkWithFile(inputWay, outputWay, key, language, flag);
         }
         output.setText(ShifrCeasar(vvod, key, language, flag));
+    }
+
+    public void SetInputFile(ActionEvent actionEvent) {
+        inputFile.setText(FileChooser());
+    }
+
+    public void SetOutputFile(ActionEvent actionEvent) {
+        outputFile.setText(FileChooser());
+    }
+
+    public String FileChooser(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выбрать файл");
+        File selectedFile = fileChooser.showOpenDialog(null);
+        return selectedFile.getPath();
     }
 }
